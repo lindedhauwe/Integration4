b952ddc87f02cd8c38efa5fcfcb21eec7e33c1c4
femke <femke.de.latter@student.howest.be>
2026-06-03 11:26:29 +0200

    per ongeluk iets op de main rechtstreeks gezet sorry

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,5 +1,6 @@
 // src/pages/Account.jsx
 import UserForm from '../components/UserForm';
+import { Link } from 'react-router-dom';
 
 export default function Account() {
     return (
@@ -7,6 +8,11 @@ export default function Account() {
             <h1>Account</h1>
 
             <UserForm />
+
+            <h2>Contact</h2>
+            <p>Questions? Contact us anytime!</p>
+            <Link to="mailto:info@antwerp.be">info@antwerp.be</Link>
+            <Link to="tel:+32 03 22 11 333">+32 03 22 11 333</Link>
         </main>
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,10 @@
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+            <main>
+                <h1>Home</h1>
+                
+    
+            
+            </main>
+        );
 }
