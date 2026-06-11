343764e7922950987b44deee91526af796c70643
naomi <naomi.desmet@student.howest.be>
2026-06-11 10:04:33 +0200

    add storytelling page and install gsap

---
+++ router-app/app/routes/storytelling.jsx
@@ -0,0 +1,15 @@
+import "./storytelling.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Storytelling() {
+    return(
+        <div class="wrapper">
+            <div class="container">
+                <section></section>
+            </div>
+        </div>
+    )
+}
