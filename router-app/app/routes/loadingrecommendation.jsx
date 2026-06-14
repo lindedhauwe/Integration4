7471926ee5446f6b815eb2897fe278cb1b5a2748
naomi <naomi.desmet@student.howest.be>
2026-06-14 17:40:44 +0200

    add loading recommendation page, add assets

---
+++ router-app/app/routes/loadingrecommendation.jsx
@@ -0,0 +1,15 @@
+import "./loadingrecommendation.css";
+import gsap from "gsap";
+
+export default function loadingRecommendation() {
+    return (
+        <>
+            <div className="loading-recommendation">
+                <div className="loading-animation">
+                    
+                </div>
+                <h1 className="loading-recommendation__title">Finding the <span>Antwerp local</span> who gets your taste...</h1>
+            </div>
+        </>
+    )
+}
